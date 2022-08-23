09ba6f358f212fb463776c68465cd826fb8efe07
farisa <farisanadia2802@gmail.com>
2022-08-23 21:09:15 +0800

    Added ability to understand dates and times

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,11 +1,31 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
+import java.util.Date;
+
 public class Deadlines extends Task {
     protected String taskType = "[D]";
     protected String deadline;
 
     public Deadlines(String description, String d) {
         super(description);
-        this.deadline = d;
+        String[] split = d.split("by ");
+        try {
+            this.deadline = LocalDate.parse(split[1], parserFormats).
+                    format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Oops! Your deadline should have a due date after /deadline");
+        } catch (DateTimeParseException e) {
+            System.out.println("Please enter a valid date!");
+        }
     }
+
+    protected static DateTimeFormatter parserFormats = new DateTimeFormatterBuilder()
+            .appendOptional(DateTimeFormatter.ISO_LOCAL_DATE)
+            .appendOptional(DateTimeFormatter.ofPattern("d MMM uuuu"))
+            .appendOptional(DateTimeFormatter.ofPattern("yyyyMMdd"))
+            .toFormatter();
 
     @Override
     public void fullDesc() {
