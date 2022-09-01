f4c25087837d503d470fa2cfbf0f0404c35bf818
farisa <farisanadia2802@gmail.com>
2022-09-01 16:18:06 +0800

    changed user and duke pics

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -6,27 +6,9 @@ public class Ui {
         System.out.println("    _____________________________________");
     }
 
-    public void printGreeting() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-
-        System.out.println("    Hello from\n" + logo);
-        System.out.println("    What can I do for you?\n");
-        printBuffLine();
-    }
-
     public void printString(String msg) {
         printBuffLine();
         System.out.println(msg);
-    }
-
-    public void printGoodbye() {
-        printBuffLine();
-        System.out.println("    Bye. Hope to see you again soon!");
-        printBuffLine();
     }
 
     public String printInvalidIndexError() {
